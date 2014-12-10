f413f192bf6443ab02564b6ab3acbe84438a4012
Larry Lv <larrylv1990@gmail.com>
2014-12-10 17:22:12 +0800

    Skip verify authenticity_token for sessions#create.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate, only: [:create]
+  skip_before_filter :verify_authenticity_token, only: :create
 
   def create
     user = find_user || create_user
