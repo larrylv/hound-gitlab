a342a86c98ab66f7b2a9158a865760fe031d4659
Larry Lv <larrylv1990@gmail.com>
2014-12-11 22:26:47 +0800

    patch_position is used for commenting.

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,7 +1,14 @@
 class Line
-  pattr_initialize :number, :content
+  attr_reader :number, :patch_position
+
+  def initialize(number:, content:, patch_position:)
+    @number = number
+    @content = content
+    @patch_position = patch_position
+  end
 
   def changed?
     true
   end
 end
+
