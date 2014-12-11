d9a58604aeac01bed8997fa2ab46712c40e269d5
Larry Lv <larrylv1990@gmail.com>
2014-12-11 16:28:03 +0800

    Add monkeypatch for Gitlab api.

---
+++ lib/gitlab/client/merge_requests.rb
@@ -0,0 +1,33 @@
+require 'gitlab'
+
+# http://doc.gitlab.com/ce/api/commits.html#post-comment-to-commit
+
+class Gitlab::Client
+  module MergeRequests
+    # Adds a comment to a merge request.
+    #
+    # @example
+    #   Gitlab.create_merge_request_comment(5, 1, "Awesome merge!")
+    #   Gitlab.create_merge_request_comment('gitlab', 1, "Awesome merge!")
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [Integer] id The ID of a merge request.
+    # @param  [String] note The content of a comment.
+    # @param  [Hash] options A customizable set of options.
+    # @option options [Integer] :line The line number
+    # @option options [String] :path The file path
+    # @option options [String] :line_type The line type (new or old)
+    # @return [Gitlab::ObjectifiedHash] Information about created merge request comment.
+    def create_merge_request_comment(project, id, note, options)
+      post("/projects/#{project}/merge_request/#{id}/comments",
+        :body => {
+          :note      => note,
+          :line      => options[:line],
+          :path      => options[:path],
+          :line_type => options[:line_type]
+        }
+      )
+    end
+  end
+end
+
