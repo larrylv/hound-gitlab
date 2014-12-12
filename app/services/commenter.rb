a7f0e6162cc3b43ec655356c39b2a9a87a1c55ba
Larry Lv <larrylv1990@gmail.com>
2014-12-12 18:01:33 +0800

    Rename merge_request to pull_request.

--- app/services/commenter.rb
+++ app/services/commenter.rb
@@ -1,11 +1,11 @@
 # Print violation messages as comments on given GitHub pull request.
 class Commenter
-  pattr_initialize :pull_request
+  pattr_initialize :merge_request
 
   def comment_on_violations(violations)
     violations.each do |violation|
       if commenting_policy.allowed_for?(violation)
-        pull_request.comment_on_violation(violation)
+        merge_request.comment_on_violation(violation)
       end
     end
   end
@@ -13,6 +13,6 @@ class Commenter
   private
 
   def commenting_policy
-    @commenting_policy ||= CommentingPolicy.new(pull_request)
+    @commenting_policy ||= CommentingPolicy.new(merge_request)
   end
 end
